c5a5d70ef02aaf50746ed8fd216449e5268c57f7
Brandon <brandon93.w@googlemail.com>
2023-08-31 03:08:10 +0200

    added sidebar

--- app/(dashboard)/(routes)/dashboard/page.tsx
+++ app/(dashboard)/(routes)/dashboard/page.tsx
@@ -1,12 +1,8 @@
-import { Button } from "@/components/ui/button";
-import { UserButton } from "@clerk/nextjs";
-
 
 const DashboardPage = () => {
   return (
     <div>
     <p>Protected Dashboard</p>
-    <UserButton afterSignOutUrl="/"/>
     </div>
   )
 }
