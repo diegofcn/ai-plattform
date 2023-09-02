7d44353333f24fb1bc6cd61e614d3af3f94c3346
Brandon <brandon93.w@googlemail.com>
2023-09-02 12:13:48 +0200

    added Counter for Free Generations

--- components/Sidebar.tsx
+++ components/Sidebar.tsx
@@ -6,6 +6,7 @@ import Image from "next/image"
 import Link from "next/link"
 import { usePathname } from "next/navigation";
 import { LayoutDashboard,MessageSquare, ImageIcon, VideoIcon, Music, Code, Settings } from "lucide-react";
+import { FreeCounter } from "./Free-Counter"
 
 const montserrat = Montserrat({weight: "600", subsets: ["latin"]})
 
@@ -54,8 +55,15 @@ const routes = [
     
 ]
 
-const Sidebar = () => {
+interface SidebarProps {
+    apiLimitCount: number;
+}
+
+const Sidebar = ({
+    apiLimitCount = 0
+}: SidebarProps) => {
     const pathname = usePathname()
+
   return (
     <div className="space-y-4 py-2 flex flex-col h-full bg-[#111827] text-white">
         <div className="px-1 py-2 flex-1">
@@ -80,6 +88,9 @@ const Sidebar = () => {
                 ))}
             </div>
         </div>
+        <FreeCounter 
+            apiLimitCount={apiLimitCount}
+        />
     </div>
   )
 }
